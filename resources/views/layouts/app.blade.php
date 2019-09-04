58995129f3765f986a54a91fce5d7f63ad5022fe
Touch <chantouchsek.cs83@gmail.com>
2019-09-04 11:20:22 +0700

    update seotool

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Booking KH') }}</title>
     {!! SEO::generate(true) !!}
     <script src="{{ asset('js/app.js') }}" defer></script>
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
